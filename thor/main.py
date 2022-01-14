2686df3478ee070d3f4c59f502cac12454456e4a
greenpea88 <green_pea88@naver.com>
2022-01-14 16:51:45 +0900

    :sparkles: NEW: post message to thread

--- thor/main.py
+++ thor/main.py
@@ -1,23 +1,53 @@
 import os
 from slack_sdk import WebClient
 
-client = WebClient(token=os.environ["TOKEN"])
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
-blocks = [{
-  "type": "section",
-  "text": {
-    "text": "*Sally* has requested you set the deadline for the Nano launch project",
-    "type": "mrkdwn"
-  },
-  "accessory": {
-    "type": "datepicker",
-    "action_id": "datepicker123",
-    "initial_date": "1990-04-28",
-    "placeholder": {
-      "type": "plain_text",
-      "text": "Select a date"
+
+def post_message():
+  blocks = [{
+    "type": "section",
+    "text": {
+      "text": "*Sally* has requested you set the deadline for the Nano launch project",
+      "type": "mrkdwn"
+    },
+    "accessory": {
+      "type": "datepicker",
+      "action_id": "datepicker123",
+      "initial_date": "1990-04-28",
+      "placeholder": {
+        "type": "plain_text",
+        "text": "Select a date"
+      }
     }
-  }
-}]
+  }]
+  response = client.chat_postMessage(channel='bot-test', blocks=blocks)
 
-response = client.chat_postMessage(channel='#bot-test', blocks=blocks)
+
+def post_message_to_thread():
+    res = client.conversations_list()
+    channels = res.data["channels"]
+    channel = list(filter(lambda c: c["name"] == "bot-test", channels))[0]
+    channel_id = channel["id"]
+
+    # conversations_history() 메서드 호출
+    result = client.conversations_history(channel=channel_id)
+    # 채널 내 메세지 정보 딕셔너리 리스트
+    messages = result.data['messages']
+    # 채널 내 메세지가 query와 일치하는 메세지 딕셔너리 쿼리
+    message = list(filter(lambda m: m["text"] == "MORE!", messages))[0]
+    # 해당 메세지ts 파싱
+    message_ts = message["ts"]
+
+    res = client.chat_postMessage(
+      channel=channel_id,
+      thread_ts=message_ts,
+      text="hello"
+    )
+
+    return res
+
+
+if __name__ == "__main__":
+  res = post_message_to_thread()
+  print(res)
--- thor/slack.py
+++ thor/slack.py
@@ -1,9 +1,36 @@
 import os
 
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 
 class SlackAPI:
     def __init__(self):
-        self.client = WebClient(token=os.environ["TOKEN"])
-#
+        self.client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+
+    def get_channel_id(self, channel_name):
+        # conversations_list() 메서드 호출
+        result = self.client.conversations_list()
+        # 채널 정보 딕셔너리 리스트
+        channels = result.data['channels']
+        # 채널 명이 'test'인 채널 딕셔너리 쿼리
+        channel = list(filter(lambda c: c["name"] == channel_name, channels))[0]
+        # 채널ID 파싱
+        channel_id = channel["id"]
+        return channel_id
+
+    def post_message(self, channel_id, text):
+        res = self.client.chat_postMessage(
+            channel=channel_id,
+            text=text
+        )
+
+        return res
+
+    def post_message_to_thread(self, channel_id, message_ts, text):
+        # 채널 내 메세지의 thread에 글 작성하기
+        res = self.client.chat_postMessage(
+            channel=channel_id,
+            thread_ts=message_ts,
+            text=text
+        )
