346efa12ffc73cf91a0925839b721c5c9b822096
greenpea88 <green_pea88@naver.com>
2022-01-14 14:41:43 +0900

    :building_construction: ADD: load token from env

--- thor/main.py
+++ thor/main.py
@@ -1,7 +1,6 @@
+import os
 from slack_sdk import WebClient
 
-TOKEN = "xoxb-2975781091040-2949213389317-8P4daKKMSlL6km3zMFgw2GfC"
+client = WebClient(token=os.environ["TOKEN"])
 
-client = WebClient(token=TOKEN)
-
-response = client.chat_postMessage(channel='#bot-test', text="Hello world!")
+response = client.chat_postMessage(channel='#bot-test', text="MORE!")
